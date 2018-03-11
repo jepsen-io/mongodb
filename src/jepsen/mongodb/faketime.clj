455287e1c0b6347ac847149e264cafa4ebb70e1b
Max Hirschhorn <max.hirschhorn@mongodb.com>
2018-03-10 21:31:33 -0500

    Add support for running tests without any virtualization.
    Moves process management related code into a separate
    `jepsen.mongodb.cluster` namespace. This namespace defines multimethods
    for each of the `init!`, `start!`, `stop!`, and `kill!` behaviors, and
    has methods associated with the `:none` and `:vm` virtualization
    options.
    
    Adds support for spawning mongobridge processes to do the network
    partitioning.
    
    Exposes a `--working-dir` option to have Jepsen extract the MongoDB
    binaries and write faketimerc configuration files into a directory other
    than `/opt/mongodb/` (or `/opt/jepsen/`).
    
    Exposes a `--mongodb-dir` option to have Jepsen install the MongoDB
    binaries from an existing checkout of the mongodb/mongo repository, or
    from an already extracted tarball.
    
    Also changes `jepsen.mongodb.faketime/upload-faketimerc!` to swap the
    faketimerc configuration file via an atomic rename. This eliminates the
    risk where the libfaketime library could misbehave if the new faketimerc
    configuration file had been partially written when read by a thread in
    mongod.

--- src/jepsen/mongodb/faketime.clj
+++ src/jepsen/mongodb/faketime.clj
@@ -1,68 +1,93 @@
 (ns jepsen.mongodb.faketime
   "Functions for messing with time and clocks."
   (:require [clj-time.core :as time]
+            [clojure.java.io :as io]
             [jepsen.control :as c]
-            [jepsen.mongodb.time :as mt])
+            [jepsen.mongodb.time :as mt]
+            [jepsen.mongodb.util :as mu])
   (:import (java.nio.charset Charset)
+           (java.nio.file CopyOption)
            (java.nio.file Files)
            (java.nio.file OpenOption)
+           (java.nio.file StandardCopyOption)
            (java.nio.file StandardOpenOption)
            (java.nio.file.attribute FileAttribute)
            (java.nio.file.attribute PosixFilePermissions)
            (org.joda.time Period)))
 
+(defn- faketime-timestamp-file
+  [test node]
+  (mu/path-prefix test node "/faketimerc"))
+
 (defn- upload-faketimerc!
-  "Updates the /opt/jepsen/faketimerc configuration file."
-  [^Period offset]
-  (c/su
-    (let [tmp-file
-          (Files/createTempFile
-            "jepsen-upload"
-            ".faketimerc"
-            (into-array FileAttribute [(PosixFilePermissions/asFileAttribute
-                                         (PosixFilePermissions/fromString
-                                           "rw-r--r--"))]))]
-      (try
-        (Files/write tmp-file
-                     ; libfaketime requires a leading '+' for setting offsets
-                     ; ahead of the true time.
-                     [(format "%+d" (time/in-seconds offset))]
-                     (Charset/defaultCharset)
-                     (into-array OpenOption [StandardOpenOption/WRITE]))
-        ; Upload
-        (c/exec :mkdir :-p "/opt/jepsen")
-        (c/exec :chmod "a+rwx" "/opt/jepsen")
-        (c/upload (str tmp-file) "/opt/jepsen/faketimerc")
-        (finally
-          (Files/delete tmp-file))))))
+  "Updates the faketimerc configuration file."
+  [test ^Period offset]
+  (let [dest     (faketime-timestamp-file test c/*host*)
+        tmp-file (Files/createTempFile
+                   "jepsen-upload"
+                   ".faketimerc"
+                   (into-array FileAttribute
+                               [(PosixFilePermissions/asFileAttribute
+                                  (PosixFilePermissions/fromString
+                                    "rw-r--r--"))]))]
+    (try
+      (Files/write tmp-file
+                   ; libfaketime requires a leading '+' for setting offsets
+                   ; ahead of the true time.
+                   [(format "%+d" (time/in-seconds offset))]
+                   (Charset/defaultCharset)
+                   (into-array OpenOption [StandardOpenOption/WRITE]))
+
+      ; Swap the faketimerc configuration file such that its contents are
+      ; atomically made visible to the underlying process.
+      (if (= :vm (:virt test))
+        ; If we're running with some form of virtualization, then we first
+        ; upload the faketimerc configuration file to the remote host (via SCP)
+        ; using its temporary name, and then atomically rename it to the
+        ; FAKETIME_TIMESTAMP_FILE name.
+        (let [tmp-file-dest
+              (mu/path-prefix test c/*host* (str "/" (.getFileName tmp-file)))]
+          (do (c/upload (str tmp-file) tmp-file-dest)
+              (c/exec :mv :-f tmp-file-dest dest)))
+
+        ; If we're running without any virtualization, then we just atomically
+        ; rename it to the FAKETIME_TIMESTAMP_FILE name.
+        (Files/move tmp-file
+                    (.toPath (io/file dest))
+                    (into-array CopyOption
+                                [StandardCopyOption/ATOMIC_MOVE
+                                 StandardCopyOption/REPLACE_EXISTING])))
+      (finally
+        (Files/deleteIfExists tmp-file)))))
 
 (defn- add-periods [a b] (.plus (.toPeriod a) (.toPeriod b)))
 
 (defn clock
   "Performs clock skewing using libfaketime and its file-based configuration
   mechanism."
-  ([{:keys [libfaketime-path]}]
-   (let [state (atom {})
-         true-time (time/seconds 0)]
-     (reify mt/Clock
-       (init! [clock]
-         (swap! state assoc c/*host* (atom true-time))
-         ; We reset the faketimerc configuration file when initializing the clock
-         ; to avoid causing heartbeat requests to time out spuriously when
-         ; initiating the replica set.
-         (upload-faketimerc! true-time))
+  [{:keys [libfaketime-path]}]
+  (let [state (atom {})
+        true-time (time/seconds 0)]
+    (reify mt/Clock
+      (init! [clock test]
+        (swap! state assoc c/*host* (atom true-time))
+        ; We reset the faketimerc configuration file when initializing the clock
+        ; to avoid causing heartbeat requests to time out spuriously when
+        ; initiating the replica set.
+        (upload-faketimerc! test true-time))
 
-       (wrap! [clock bin]
-         (conj ["/usr/bin/env"
-                "FAKETIME_NO_CACHE=1"
-                "FAKETIME_TIMESTAMP_FILE=/opt/jepsen/faketimerc"
-                (format "LD_PRELOAD=%s" libfaketime-path)]
-                bin))
+      (wrap! [clock test bin]
+        (conj ["/usr/bin/env"
+               "FAKETIME_NO_CACHE=1"
+               (format "FAKETIME_TIMESTAMP_FILE=%s"
+                       (faketime-timestamp-file test c/*host*))
+               (format "LD_PRELOAD=%s" libfaketime-path)]
+               bin))
 
-       (bump-time! [clock delta]
-         (upload-faketimerc! (swap! (get @state c/*host*)
-                                    (partial add-periods delta))))
+      (bump-time! [clock test delta]
+        (upload-faketimerc! test (swap! (get @state c/*host*)
+                                        (partial add-periods delta))))
 
-       (reset-time! [clock]
-         (upload-faketimerc! (swap! (get @state c/*host*)
-                                    (constantly true-time))))))))
+      (reset-time! [clock test]
+        (upload-faketimerc! test (swap! (get @state c/*host*)
+                                        (constantly true-time)))))))
--- src/jepsen/mongodb/time.clj
+++ src/jepsen/mongodb/time.clj
@@ -6,35 +6,35 @@
 
 (defprotocol Clock
   (init!
-    [clock]
+    [clock test]
     "Initializes any internal structures for time keeping on the current host.")
 
   (wrap!
-    [clock bin]
+    [clock test bin]
     "Returns how to invoke the specified binary under the control of this
     clock.")
 
   (bump-time!
-    [clock ^Period delta]
+    [clock test ^Period delta]
     "Advances the current host's clock by the specified time period.")
 
-  (reset-time! [clock]
+  (reset-time! [clock test]
                "Resets the current host's clock back to the true time."))
 
 (defn noop-clock
   "Does nothing."
-  ([_]
-   (reify Clock
-     (init! [clock])
-     (wrap! [clock bin] [bin])
-     (bump-time! [clock delta])
-     (reset-time! [clock]))))
+  [_]
+  (reify Clock
+    (init! [clock test])
+    (wrap! [clock test bin] [bin])
+    (bump-time! [clock test delta])
+    (reset-time! [clock test])))
 
 (defn system-clock
   "Changes the system's time using settimeofday()."
-  ([_]
-   (reify Clock
-     (init! [clock])
-     (wrap! [clock bin] [bin])
-     (bump-time! [clock delta] (nt/bump-time! (time/in-millis delta)))
-     (reset-time! [clock] (nt/reset-time!)))))
+  [_]
+  (reify Clock
+    (init! [clock test])
+    (wrap! [clock test bin] [bin])
+    (bump-time! [clock test delta] (nt/bump-time! (time/in-millis delta)))
+    (reset-time! [clock test] (nt/reset-time!))))
