74535d70fe28ad4fc58ae49946e8029d1b810211
Aphyr <aphyr@aphyr.com>
2018-07-02 14:13:07 -0500

    version 0.2.3, jepsen 0.1.10

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jepsen.mongodb "0.2.3-SNAPSHOT"
+(defproject jepsen.mongodb "0.2.3"
   :description "Jepsen MongoDB tests"
   :url "https://github.com/jepsen-io/mongodb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [jepsen "0.1.10-SNAPSHOT"]
+                 [jepsen "0.1.10"]
                  [org.mongodb/mongodb-driver "3.6.3"]]
   :jvm-opts ["-Xmx16g"
              "-Xms16g"
