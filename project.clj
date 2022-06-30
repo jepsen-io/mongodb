aee1cbd8a9c121e33c5cb9fe7ae40df3d7e4ff00
Aphyr <aphyr@aphyr.com>
2022-06-30 15:18:49 -0400

    Jepsen 0.2.7

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jepsen.mongodb "0.3.2-SNAPSHOT"
+(defproject jepsen.mongodb "0.3.2"
   :description "Jepsen MongoDB tests"
   :url "http://github.com/jepsen-io/mongodb"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-wallhack "1.0.1"]
-                 [jepsen "0.2.7-SNAPSHOT"]
+                 [jepsen "0.2.7"]
                  [org.mongodb/mongodb-driver-sync "4.6.0"]]
   :main jepsen.mongodb
   :jvm-opts ["-Djava.awt.headless=true"]
